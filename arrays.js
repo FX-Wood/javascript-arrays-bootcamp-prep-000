a514df1d709d0b34d0d5f42a54e097c2f648884f
FX Wood <fxwood2@gmail.com>
2018-11-10 20:42:14 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  console.log(array)
+}
+
+function addElementToBeginningOfArray(array, element) {
+  array.push(element)
+  console.log(array)
+}
