9827791b253c72b0249da88e8b94fb4d31620b34
FX Wood <fxwood2@gmail.com>
2018-11-10 21:16:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,20 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  const newArray = [element, ...array]
-  return newArray
+  const newArray = [element, ...array];
+  return newArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+  const newArray = array.unshift(element);
+  return newArray;
+}
+
+function addElementToEndOfArray(array, element) {
+  const newArray = [array, ...element];
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element);
 }
