7eefdeef7a104bca2238cd2beeaf388965676a82
FX Wood <fxwood2@gmail.com>
2018-11-10 21:10:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  const newArray = array
-  newArray.unshift(element)
-  return newArray
+  const biggerArray = array
+  biggerArray.unshift(element)
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   return array.unshift(element)
-
 }
